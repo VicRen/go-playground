b1c6a1950b1c6f7d2fa649e8ef37c1cc17710516
VicRen <vic.ren@me.com>
2018-12-29 13:40:46 +0800

    添加 yaml 使用 demo

---
+++ yaml/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+type TestYAML struct {
+	MySql MySql
+	Cache Cache
+}
+
+type MySql struct {
+	User     string `yaml:"user"`
+	Host     string `yaml:"host"`
+	Password string `yaml:"password"`
+	Port     string `yaml:"port"`
+	Name     string `yaml:"name"`
+}
+
+type Cache struct {
+	Enable bool     `yaml:"enable"`
+	List   []string `yaml:"list,flow"`
+}
+
+func main() {
+	var conf *TestYAML
+	f, err := ioutil.ReadFile("./yaml/test.yaml")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("--- t file:\n%s\n\n", string(f))
+
+	err = yaml.Unmarshal(f, &conf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("conf: %v\n\n", conf)
+
+	d, err := yaml.Marshal(&conf)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("--- t dump:\n%s\n\n", string(d))
+	err = ioutil.WriteFile("./yaml/test_dump.yaml", d, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+}

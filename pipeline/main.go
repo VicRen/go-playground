9ccbe7a575613b81843d77dda7116bd77a6a2366
VicRen <vic.ren@me.com>
2019-02-19 09:47:13 +0800

    尝试 pipeline

--- pipeline/main.go
+++ pipeline/main.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
+	squares := square(naturals())
+	for x := range squares {
+		fmt.Println(x)
+	}
+}
+
+func naturals() chan int {
 	naturals := make(chan int)
-	squares := make(chan int)
 
 	go func() {
 		for i := 0; i < 10; i++ {
@@ -12,7 +18,11 @@ func main() {
 		}
 		close(naturals)
 	}()
+	return naturals
+}
 
+func square(naturals chan int) chan int {
+	squares := make(chan int)
 	go func() {
 		for {
 			x, ok := <-naturals
@@ -23,8 +33,5 @@ func main() {
 		}
 		close(squares)
 	}()
-
-	for x := range squares {
-		fmt.Println(x)
-	}
+	return squares
 }

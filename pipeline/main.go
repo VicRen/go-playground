8a44984775621712b72d500f644d0f5871503931
VicRen <vic.ren@me.com>
2019-02-19 14:11:44 +0800

    fan-in 应用

--- pipeline/main.go
+++ pipeline/main.go
@@ -1,19 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"sync"
+)
 
 func main() {
-	squares := square(naturals())
-	for x := range squares {
+	n := naturals(10)
+	f := intToFloat64(square(n))
+	s1 := squareRoot(f)
+	s2 := squareRoot(f)
+	s3 := squareRoot(f)
+	out := merge(s1, s2, s3)
+	for x := range out {
 		fmt.Println(x)
 	}
 }
 
-func naturals() chan int {
+func naturals(count int) <-chan int {
 	naturals := make(chan int)
 
 	go func() {
-		for i := 0; i < 10; i++ {
+		for i := 0; i < count; i++ {
 			naturals <- i
 		}
 		close(naturals)
@@ -21,17 +30,58 @@ func naturals() chan int {
 	return naturals
 }
 
-func square(naturals chan int) chan int {
+func square(naturals <-chan int) <-chan int {
 	squares := make(chan int)
 	go func() {
-		for {
-			x, ok := <-naturals
-			if !ok {
-				break
-			}
+		defer close(squares)
+		for x := range naturals {
 			squares <- x * x
 		}
-		close(squares)
 	}()
 	return squares
+}
+
+func intToFloat64(intChan <-chan int) <-chan float64 {
+	c := make(chan float64)
+	go func() {
+		defer close(c)
+		for x := range intChan {
+			c <- float64(x)
+		}
+	}()
+	return c
+}
+
+func squareRoot(number <-chan float64) <-chan int {
+	squareRoots := make(chan int)
+	go func() {
+		defer close(squareRoots)
+		for x := range number {
+			squareRoots <- int(math.Sqrt(x))
+		}
+	}()
+	return squareRoots
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	out := make(chan int)
+
+	var wg sync.WaitGroup
+	collect := func(in <-chan int) {
+		defer wg.Done()
+		for n := range in {
+			out <- n
+		}
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go collect(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
 }

2c6a02ea63a487cb95adbc5808829e7ddb598670
VicRen <vic.ren@me.com>
2019-07-09 13:12:37 +0800

    update udp echo

---
+++ echo/udp/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	go func() {
+		log.Fatal(echoServer())
+	}()
+
+	log.Fatal(client())
+}
+
+func client() error {
+	c, e := net.Dial("udp", "192.168.101.47:6060")
+	if e != nil {
+		return e
+	}
+
+	_, err := c.Write([]byte{0x00})
+	if err != nil {
+		return err
+	}
+	fmt.Printf("client send: %v->%v\n", c.LocalAddr(), c.RemoteAddr())
+
+	var b [65535]byte
+	n, err := c.Read(b[:])
+	if err != nil {
+		fmt.Println("client read:", err)
+		return err
+	}
+	d := b[:n]
+	fmt.Printf("client recv: %v->%v: %v\n", c.RemoteAddr(), c.LocalAddr(), d)
+	return nil
+}
+
+func echoServer() error {
+	c, e := net.ListenUDP("udp", &net.UDPAddr{Port: 6060})
+	if e != nil {
+		return e
+	}
+	for {
+		var b [65535]byte
+		n, addr, err := c.ReadFrom(b[:])
+		if err != nil {
+			fmt.Println("server read:", err)
+			return err
+		}
+		d := b[:n]
+		fmt.Printf("server recv: %v->%v: %v\n", addr, c.LocalAddr(), d)
+
+		//conn, err := net.Dial("udp", addr.String())
+		//if err != nil {
+		//	return err
+		//}
+		_, err = c.WriteTo(d, addr)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("server send: %v->%v: %v\n", c.LocalAddr(), addr, d)
+	}
+}
